e00157c103980469bcc825698faf62da560aedd6
mrChernicharo <felipe.chernicharo@gmail.com>
2020-07-28 16:25:49 -0300

    7th assignment - my solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,52 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  @ViewChild('f', { static: false }) formData: NgForm;
-  subscriptions = ['Basic', 'Advanced', 'Pro'];
-  data = {
-    email: '',
-    subscription: '',
-    password: ''
-  }
-  submitted = false
+export class AppComponent implements OnInit {
+  projectForm: FormGroup;
+  statusArray = ['Stable', 'Critical', 'Finished'];
+  unallowedProject = ['Test', 'Teste'];
+  unallowedEmails = ['test@test.com', 'string7dev@gmail.com'];
 
-  onSubmit(form: NgForm) {
-    console.log(this.formData.value)
-    this.data.email = this.formData.value.email;
-    this.data.subscription = this.formData.value.subscription;
-    this.data.password = this.formData.value.password;
-    this.submitted = true;
-    setTimeout(() => {
-      this.submitted = false
-    }, 3000)
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null, this.unallowedProjectName.bind(this)),
+      'email': new FormControl(null, [Validators.required, Validators.email], this.unallowedEmail),
+      'status': new FormControl(null)
+    })
+    this.projectForm.statusChanges.subscribe(
+      (status) => console.log(status)
+    )
+  }
+
+  onSubmit() {
+    console.log(this.projectForm)
+  }
+
+  unallowedProjectName(control: FormControl): {[key: string]: boolean} {
+    if (this.unallowedProject.find(item => item === control.value)) {
+      return {'nameIsUnallowed': true};
+    } else {
+      return null;
+    }
+  }
+
+  unallowedEmail(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({emailIsUnallowed: true})
+        } else {
+          resolve(null)
+        }
+      }, 2000)
+    })
+    return promise;
   }
 }
