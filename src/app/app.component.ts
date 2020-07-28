d113dfe7c497e92dff7412d2c33e8515812d8428
mrChernicharo <felipe.chernicharo@gmail.com>
2020-07-27 21:54:43 -0300

    6th assignment - template-driven forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f', { static: false }) formData: NgForm;
+  subscriptions = ['Basic', 'Advanced', 'Pro'];
+  data = {
+    email: '',
+    subscription: '',
+    password: ''
+  }
+  submitted = false
+
+  onSubmit(form: NgForm) {
+    console.log(this.formData.value)
+    this.data.email = this.formData.value.email;
+    this.data.subscription = this.formData.value.subscription;
+    this.data.password = this.formData.value.password;
+    this.submitted = true;
+    setTimeout(() => {
+      this.submitted = false
+    }, 3000)
+  }
 }
